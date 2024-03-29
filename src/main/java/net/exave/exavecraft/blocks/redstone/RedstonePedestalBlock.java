d130ba169faa1b132ec3ac928a0536cfbeeb0764
exceedinglyaverage <jphilward@gmail.com>
2024-03-29 09:35:27 -0700

    3/29/2024

---
+++ src/main/java/net/exave/exavecraft/blocks/redstone/RedstonePedestalBlock.java
@@ -0,0 +1,33 @@
+package net.exave.exavecraft.blocks.redstone;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class RedstonePedestalBlock extends Block {
+    public RedstonePedestalBlock(Settings settings) {
+        super(settings);
+    }
+    @Override
+    public boolean emitsRedstonePower(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        return 15;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable BlockView world, List<Text> tooltip, TooltipContext options) {
+        tooltip.add(Text.translatable("tooltip.exavecraft.wip"));
+    }
+}
